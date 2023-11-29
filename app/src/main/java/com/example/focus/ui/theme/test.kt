9646f5d483fd8d735cb51ed2d6c57332418c3d64
hieucoolngau <hieu57075@gmail.com>
2023-11-29 14:17:31 +0700

    Build UI pormodo App

--- app/src/main/java/com/example/focus/ui/theme/test.kt
+++ app/src/main/java/com/example/focus/ui/theme/test.kt
@@ -1,15 +1,12 @@
 package com.example.focus.ui.theme
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,17 +16,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.focus.R
-import java.lang.Math.atan2
-import java.lang.Math.cos
-import java.lang.Math.sin
 import kotlin.math.atan2
+
 @Composable
 fun Content() {
 
@@ -49,6 +41,7 @@ fun Content() {
         mutableStateOf(0.0)
     }
 
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
@@ -66,9 +59,10 @@ fun Content() {
         shapeCenter = center
         radius = size.minDimension / 2
 
-        val x = (shapeCenter.x + cos(Math.toRadians(angle)) * radius).toFloat()
-        val y = (shapeCenter.y + sin(Math.toRadians(angle)) * radius).toFloat()
+        val x = (shapeCenter.x + kotlin.math.cos(Math.toRadians(angle)) * radius).toFloat()
+        val y = (shapeCenter.y + kotlin.math.sin(Math.toRadians(angle)) * radius).toFloat()
         handleCenter = Offset(x, y)
+
         drawCircle(color = Color(0xDFC0CC82),  radius = radius)
         drawCircle(color = Color(0xFFFAEB9F),  radius = radius, style = Stroke(20f))
         drawArc(
@@ -80,9 +74,12 @@ fun Content() {
         )
 
         drawCircle(color = Color(0xDF80DF2D), center = handleCenter, radius = 35f)
-    }
-    Text(text = "angle:$angle")
 
+    }
+    Column {
+        Text(text = "handleCenter:$handleCenter")
+        Text(text = "Radius$radius")
+    }
 
 }
 
@@ -102,9 +99,8 @@ private fun getRotationAngle(currentPosition: Offset, center: Offset): Double {
 @Preview (showSystemUi = true, showBackground = true , backgroundColor =0xFF4F6F52,
     device = "spec:width=1080px,height=2340px,dpi=440"
 )
-
 @Composable
-fun previewContent()
+fun PreviewContent()
 {
     Content()
 }
@@ -123,7 +119,28 @@ fun CirlcleCanvas()
             color = Color.Cyan,
             startAngle = 0f,
             sweepAngle = 90f,
-            useCenter = true
+            useCenter = true,
+            style = Stroke(20f)
         )
     }
 }
+@Preview (showBackground = true, showSystemUi = true)
+@Composable
+fun CirlcleCanvas2()
+{
+    Canvas(
+        modifier = Modifier
+            .border(color = Color.Magenta, width = 2.dp)
+            .aspectRatio(1f)
+            .padding(30.dp)
+    ) {
+        drawCircle(color = Color(0xFFFAEB9F),  radius = size.minDimension / 2, style = Stroke(20f))
+        drawArc(
+            color = Color.Cyan,
+            startAngle = -90f,
+            sweepAngle = 180f,
+            useCenter = false
+        )
+    }
+}
+
